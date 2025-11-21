bf39037a42e12007df43bfb46429bdc5985ae9cb
azmccormicks-beep <azmccormicks@gmail.com>
2025-11-21 12:15:26 -0700

    fresh

--- books.js
+++ books.js
@@ -1,33 +1,33 @@
-function renderBooks(filter) {
-          const booksWrapper = document.querySelector(".books");
+  function renderBooks(filter) {
+      const booksWrapper = document.querySelector(".books");
 
-          const books = getBooks();
+      const books = getBooks();
 
-          if (filter === "LOW_TO_HIGH") {
-            books.sort((a, b) => a.originalPrice - b.originalPrice);
-          } else if (filter === "HIGH_TO_LOW") {
-            books.sort((a, b) => b.originalPrice - a.originalPrice);
-          } else if (filter === "RATING") {
-            books.sort((a, b) => b.rating - a.rating);
-          }
+      if (filter === "LOW_TO_HIGH") {
+        books.sort((a, b) => a.originalPrice - b.originalPrice);
+      } else if (filter === "HIGH_TO_LOW") {
+        books.sort((a, b) => b.originalPrice - a.originalPrice);
+      } else if (filter === "RATING") {
+        books.sort((a, b) => b.rating - a.rating);
+      }
 
-            const booksHtml = books.map((book) => {
-            
-            const priceHtml = book.salePrice
-            ?`<span class="book__price--normal">$${book.originalPrice.toFixed(2)}</span>
+        const booksHtml = books.map((book) => {
+        
+        const priceHtml = book.salePrice
+          ?`<span class="book__price--normal">$${book.originalPrice.toFixed(2)}</span>
             <span class="book__price--sale">$${book.salePrice.toFixed(2)}</span>`
-            :`$${book.originalPrice.toFixed(2)}`
+          :`$${book.originalPrice.toFixed(2)}`
 
-            return `<div class="book"
-                <figure class="book__img--wrapper>
-                    <img class="book__img" src="${book.url}" alt="${book.title}">
-                </figure>
-                <div class="book__title">${book.title}/div>
-                <div class="book__ratings">${ratingsHTML(book.rating)}/div>
-                <div class="book__price ${priceHtml}</div>
-            </div>;`
-          
-          })
+        return  `<div class="book"
+            <figure class="book__img--wrapper>
+                <img class="book__img" src="${book.url}" alt="">
+            </figure>
+            <div class="book__title">${book.title}
+            <div class="book__ratings">${ratingsHTML(book.rating)}
+            <div class="book__price ${priceHtml}
+        </div>;`
+      
+      })
       
    
     .join("");

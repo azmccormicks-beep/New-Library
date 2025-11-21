ccaa2065d04ca6643d7780a5167301acaff8ecf6
azmccormicks-beep <azmccormicks@gmail.com>
2025-11-21 10:14:19 -0700

    updated

--- books.js
+++ books.js
@@ -1,43 +1,39 @@
 function renderBooks(filter) {
-  const booksWrapper = document.querySelector(".books");
-  
-  const books = getBooks();
-  
+          const booksWrapper = document.querySelector(".books");
 
-  if (filter === "LOW_TO_HIGH") {
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
-  } else if (filter === "HIGH_TO_LOW") {
-    books.sort((a, b) => b.originalPrice - a.originalPrice);
-  } else if (filter === "RATING") {
-    books.sort((a, b) => b.rating - a.rating);
-  }
+          const books = getBooks();
 
-    const booksHtml = books.map((book) => {
-         return `<div class="book">
-        <figure class="book__img--wrapper>
-          <img class="book__img" src="${book.url}" alt="">
-        </figure>
-        <div class="book__title">
-          ${book.title}
-        </div>
-        <div class="book__ratings">
-          ${ratingsHTML(book.rating)}
-        </div>
-        <div class="book__price">
-          <span class="book__price--normal">$${book.originalPrice.toFixed(
-            2
-          )}</span>
-     </div>`;
-      })
-    
-      .join("");
- 
-       
-    booksWrapper.innerHTML = booksHtml;
- 
- }
-  
+          if (filter === "LOW_TO_HIGH") {
+            books.sort((a, b) => a.originalPrice - b.originalPrice);
+          } else if (filter === "HIGH_TO_LOW") {
+            books.sort((a, b) => b.originalPrice - a.originalPrice);
+          } else if (filter === "RATING") {
+            books.sort((a, b) => b.rating - a.rating);
+          }
 
+            const booksHtml = books.map((book) => {
+            
+            const priceHtml = book.salePrice
+            ?`<span class="book__price--normal">$${book.originalPrice.toFixed(2)}</span>
+            <span class="book__price--sale">$${book.salePrice.toFixed(2)}</span>`
+            :`$${book.originalPrice.toFixed(2)}`
+
+            return `<div class="book"
+                <figure class="book__img--wrapper>
+                    <img class="book__img" src="${book.url}" alt="${book.title}">
+                </figure>
+                <div class="book__title">${book.title}/div>
+                <div class="book__ratings">${ratingsHTML(book.rating)}/div>
+                <div class="book__price ${priceHtml}</div>
+            </div>;`
+          
+          })
+      
+   
+    .join("");
+
+  booksWrapper.innerHTML = booksHtml;
+}
 
 function ratingsHTML(rating) {
   let ratingHTML = "";
@@ -55,7 +51,7 @@ function filterBooks(event) {
 }
 setTimeout(() => {
   renderBooks();
-},0);
+}, 0);
 
 // FAKE DATA
 function getBooks() {
